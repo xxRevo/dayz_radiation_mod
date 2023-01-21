3fb514d3f34fd81beea201f5b4b4dbda98e446f5
Revo <berkem_elgul@hotmail.com>
2023-01-21 21:17:36 +0300

    removing pmk5

--- RadiationMetro/scripts/4_World/GasMask/GasMask.c
+++ RadiationMetro/scripts/4_World/GasMask/GasMask.c
@@ -478,34 +478,3 @@ modded class AirborneMask
 	}
 
 };
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/*class PMK_5A_Gas_Mask : ClothingBase
-{
-	override bool CanPutAsAttachment( EntityAI parent )
-	{
-		if(!super.CanPutAsAttachment(parent)) {return false;}
-		bool headgear_present = false;
-		
-		if ( parent.FindAttachmentBySlotName( "Headgear" ) )
-		{
-			headgear_present = parent.FindAttachmentBySlotName( "Headgear" ).ConfigGetBool( "noMask" );
-		}
-		
-		if ( ( GetNumberOfItems() == 0 || !parent || parent.IsMan() ) && !headgear_present )
-		{
-			return true;
-		}
-		return false;
-	}
-
-	override bool IsObstructingVoice()
-	{
-		return true;
-	}
-
-	override int GetVoiceEffect()
-	{
-		return VoiceEffectObstruction;
-	}
-}*/
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
